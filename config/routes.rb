fad2b74bf70e983cf5ca4b3bce5b13a4802863b4
Matias Mascazzini <matiasmasca@gmail.com>
2016-10-22 14:46:55 -0300

    Adding a html template for the home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#paginas estaticas
+get 'pages/index'
+
+root to: "pages#index"
+ 
 end
